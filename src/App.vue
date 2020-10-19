d846cd879898ef1ec1ec6febd91e5740629d59d5
yudachao <290945266@qq.com>
2020-10-20 00:05:20 +0800

    feat: 完成global-headerglobal-header组件

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app container">
+    <global-header :user="currentUser" />
     <column-list :list="list" />
   </div>
 </template>
@@ -8,10 +9,12 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from '@/components/column-list.vue'
+import GlobalHeader, { UserProps } from '@/components/global-header.vue'
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup () {
     const list: ColumnProps[] = [
@@ -39,8 +42,13 @@ export default defineComponent({
         description: '这是一段测试数据'
       }
     ]
+    const currentUser: UserProps = {
+      isLogin: true,
+      name: 'Viking'
+    }
     return {
-      list
+      list,
+      currentUser
     }
   }
 })
---
+++ src/components/global-header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4 justify-content-between">
+    <a class="navbar-brand" href="#">Vue3 知乎</a>
+    <ul v-if="!user.isLogin" class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light">登陆</a>
+      </li>
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light">注册</a>
+      </li>
+    </ul>
+    <ul v-else class="list-inline mb-0">
+      <li class="list-inline-item">
+        <a href="#" class="btn btn-outline-light">你好，{{ user.name }}</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+export interface UserProps {
+  isLogin: boolean;
+  name?: string;
+  id?: string;
+}
+export default defineComponent({
+  name: 'global-header',
+  props: {
+    user: {
+      type: Object as PropType<UserProps>,
+      required: true
+    }
+  }
+})
+</script>
